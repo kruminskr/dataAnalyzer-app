ff8e87ac2a2d83ad171928e9dae12c35bd37bd16
kruminskr <kruminskr@gmail.com>
2025-08-07 18:43:01 +0300

    fix: add allowed hosts

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+    host: true,
+    port: 4173,
+    allowedHosts: ['project-app-4idz.onrender.com']
+  }
 })
